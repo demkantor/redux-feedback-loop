f40ca74082c747e1d4332e47291970171fce2241
demkantor <demkantor@gmail.com>
2020-02-29 14:44:19 -0600

    server post complete

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,12 +1,40 @@
 import React, {Component} from 'react';
 import '../App/App.css';
 import {connect} from 'react-redux';
+import axios from 'axios';
 
 class Review extends Component {
 
   handleClick =()=>{
+    const feedback = this.props.reduxState.responseReducer
+  let objectToSend = {
+       feeling: Number(feedback[0].feeling),
+       understanding: Number(feedback[1].understanding),
+       supporting: Number(feedback[2].supporting),
+       comments: feedback[3].comments
+     } 
+     console.log('this is objectToSend', objectToSend);    
+    this.axiosPostFunction(objectToSend);
+  } 
+
+  axiosPostFunction=(input)=>{
+    console.log('this is input', input);
+    axios.post('/api/response', input)
+   .then( response => {
+     console.table(response);
+   })
+   .catch( error => {
+     alert(`Couldn't submit responses at this time`);
+     console.log('Error posting to feedback', error);
+   })
+    this.nextPage();
+  }
+
+
+  nextPage =()=>{
     this.props.history.push('/submitted');
   }
+
 
 
 render() {
